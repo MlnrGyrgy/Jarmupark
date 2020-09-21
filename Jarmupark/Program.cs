0bff0c9af3bdd8d3589543ff3d36671fa8525d1b
MlnrGyrgy <molnar.gyorgy.mrgr@gmail.com>
2020-09-21 13:04:03 +0200

    Mukodo auto osztaly

--- Jarmupark/Program.cs
+++ Jarmupark/Program.cs
@@ -10,6 +10,20 @@ namespace Jarmupark
     {
         static void Main(string[] args)
         {
+            Auto lada = new Auto("piros", "Lada 2112", 11);
+            Auto trabant= new Auto("zöld","Trabant 1.1",8);
+
+            lada.Tankol(20);
+            trabant.Tankol(20);
+
+            lada.Allapot();
+            trabant.Allapot();
+            Console.WriteLine("Mennyit megy a Lada? ");
+            int km = int.Parse(Console.ReadLine());
+            lada.Megy(km);
+            lada.Allapot();
+            
+            Console.ReadKey();
         }
     }
 }
